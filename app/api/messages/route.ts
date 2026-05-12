0dfe55e15b9465613dd8e05e6fc67b8c5bf474d4
stagiaire <stagiaire@FORMATION.LOCAL>
2026-05-12 14:41:08 +0400

    Fix sécurité

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -52,12 +52,6 @@ export async function POST(request: NextRequest) {
     userId: session.user.id,
     userName: session.user.name,
   };
-  if (!content || content.length > 10) {
-  return Response.json(
-    { error: "Max 10 caractères" },
-    { status: 400 }
-  );
-}
   await db.collection("messages").insertOne(message);
   return NextResponse.json(message, { status: 201 });
 }
